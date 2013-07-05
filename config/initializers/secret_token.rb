a112669ea22c597a439d1ebcefa0fb2033f34590
sukendhar <sukendhar555@gmail.com>
2013-07-05 23:25:38 +0530

    first commit application setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MineApp::Application.config.secret_token = 'eac58987edd212d43dad6cb83ec7f4a13ac06034d00987351fe024d2f5f695de0f6fc8c593a2d04efc10d8fe6c4d5f00da7d211539393e5d3345e7cb37522a88'
